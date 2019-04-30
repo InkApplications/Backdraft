84afe6e503a100729ef2ff75c1b1c23ff60f5b84
Renee Vandervelde <Renee@ReneeVandervelde.com>
2019-04-30 18:11:22 -0500

    Add ID Token Flow

--- auth/src/main/java/inkapplications/backdraft/auth/AuthCoroutines.kt
+++ auth/src/main/java/inkapplications/backdraft/auth/AuthCoroutines.kt
@@ -1,9 +1,14 @@
 package inkapplications.backdraft.auth
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GetTokenResult
+import inkapplications.backdraft.tasks.await
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowViaChannel
+import kotlinx.coroutines.launch
 
 /**
  * Flow that receives changes in Firebase's Authentication state.
@@ -16,3 +21,26 @@ val FirebaseAuth.authStateFlow: Flow<FirebaseAuth> get() = flowViaChannel { chan
     addAuthStateListener(listener)
     channel.invokeOnClose { removeAuthStateListener(listener) }
 }
+
+/**
+ * Flow that receives changes in Firebase's ID Tokens.
+ *
+ * @see FirebaseAuth.addIdTokenListener
+ */
+@FlowPreview
+val FirebaseAuth.idTokenFlow: Flow<GetTokenResult?> get() = flowViaChannel { channel ->
+    val job = Job()
+    val listener = FirebaseAuth.IdTokenListener {
+        GlobalScope.launch(job) {
+            val token = it.currentUser?.getIdToken(false)?.await()
+            channel.send(token)
+        }
+    }
+    addIdTokenListener(listener)
+
+    channel.invokeOnClose {
+        removeIdTokenListener(listener)
+        job.cancel()
+    }
+}
+
